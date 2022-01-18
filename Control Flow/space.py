8c200b6a90a66dfd79c4f650b81cb2aef97030c7
m0nksecc <monksec@protonmail.com>
2022-01-17 23:53:35 -0500

    Create space.py

---
+++ Control Flow/space.py
@@ -0,0 +1,24 @@
+#Little Codey is an interplanetary space boxer, who is trying to win championship belts for various weight categories on other planets within the solar system.
+
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+
+weight = 185
+planet = 3
+
+# Write an if statement below:
+if planet == 1:
+  print(weight * 0.91)
+if planet == 2:
+  print(weight * 0.38)  
+if planet == 3:
+  print(weight * 2.34)
+if planet == 4:
+  print(weight * 1.06)
+if planet == 5:
+  print(weight * 0.92)
+if planet == 5:
+  print(weight * 1.19)  
