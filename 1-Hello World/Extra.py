f70c4d970907103b4052ac63f5966497ca6ddf1c
m0nksecc <monksec@protonmail.com>
2022-01-17 22:45:52 -0500

    Create Extra.py

---
+++ 1-Hello World/Extra.py
@@ -0,0 +1,14 @@
+#Practing print statements and string concatenation
+print("Hello, my name is ")
+name = input("What is your name?")
+print("Nice to meet you" + name)
+print("In ASCII it would be written like this:")
+#
+print(" SSS           GGG")
+print("S   S         G   G")
+print("S             G")
+print(" SSS          G  GGG")
+print("    S         G    G"  )
+print("S   S         G    G")
+print(" SSS           GGG")
+
